78533b57757b1601c777ce524f1927e3f8d61acf
Surajit00007 <surajitcoc121@gmail.com>
2026-05-14 21:43:35 +0530

    Initial commit: Library Loan Management System

---
+++ src/surajit_2341019165/util/ReportGenerator.java
@@ -0,0 +1,21 @@
+package surajit_2341019165.util;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ReportGenerator {
+    public void writeCsv(Path path, List<String> rows) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+            writer.write("Operation,Records,ExecutionTime(ms),Throughput");
+            writer.newLine();
+            for (String row : rows) {
+                writer.write(row);
+                writer.newLine();
+            }
+        }
+    }
+}
